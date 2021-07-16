82144f56ce592e72346abb06168f16ffb370f239
FIRST_NAME LAST_NAME <kapil.sharma@vconnekt.com>
2021-07-16 22:23:16 +0530

    add 90's year

--- src/desawarGame.js
+++ src/desawarGame.js
@@ -2,18 +2,16 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import "./App.css";
 
-const years = {
-  Y_1980: require("./desawarYears/1980.json"),
-  // Y_1981: require("./desawarYears/1980.json"),
-  Y_1982: require("./desawarYears/1982.json"),
-  Y_1990: require("./desawarYears/1990.json"),
-  // Y_1984: require("./desawarYears/1984.json"),
-  // Y_1985: require("./desawarYears/1985.json"),
-  // Y_1986: require("./desawarYears/1986.json"),
-  // Y_1987: require("./desawarYears/1987.json"),
-  // Y_1988: require("./desawarYears/1988.json"),
-  // Y_1989: require("./desawarYears/1989.json"),
-};
+const context = require.context("./desawarYears/", true, /.json$/);
+const years = {};
+context.keys().forEach((key) => {
+  const fileName = key.replace("./", "");
+  const resource = require(`./desawarYears/${fileName}`);
+  const namespace = fileName.replace(".json", "");
+  years[namespace] = JSON.parse(JSON.stringify(resource));
+});
+
+console.log(years);
 
 const DesawarGame = () => {
   const months = [
@@ -31,6 +29,7 @@ const DesawarGame = () => {
     "NOV",
     "DEC",
   ];
+  console.log(months);
   // const [months, setmonths] = useState([...months]);
 
   // const [currentYear, setCurrentYear] = useState(years);
@@ -103,9 +102,9 @@ const DesawarGame = () => {
     return Object.keys(years).map((item, key) => {
       return (
         <p style={{ marginTop: 40 }}>
-          <h1>{item.split("_")[1]}</h1>
+          <h1>{item}</h1>
           <p style={{ margin: 0 }}>
-            {latestMonth(item.split("_")[1]).map((value, key) => {
+            {latestMonth(item).map((value, key) => {
               return (
                 <span key={key} className="span_box_fix">
                   {value}
